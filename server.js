91a30faa33d299fadbb8eb011d5f0ebb764e282a
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-18 14:05:11 +0530

    tried manual path join

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-if(process.env.NODE_ENV === "production"){
+
 
     app.use(express.static("client/build"))
     app.get("*" , (req , res)=>{
-        res.sendFile(path.join(__dirname , "client" ,"build" , "index.html"));
+        res.sendFile(__dirname + "/client/build/index.html");
     })
-}
+
 
 const port = process.env.PORT || 3500;
 
