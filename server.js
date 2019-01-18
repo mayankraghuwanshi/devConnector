a2fa4bf5d2b31277e86266830cfe1dbbfe3d1e01
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-18 13:06:32 +0530

    run build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const dotenv = require('dotenv').config
 const path = require('path')
 var cors = require('cors')
 
