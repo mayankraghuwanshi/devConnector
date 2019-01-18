c29e7ec7d95f6525fcc576819060dcf741d1000d
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-18 13:24:37 +0530

    .

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.resolve("./client/build"))
+if(process.env.NODE_ENV === "production"){
+
+    app.use(express.static("./client/build"))
     app.get("*" , (req , res)=>{
         res.sendFile(path.resolve(__dirname , "client" ,"build" , "index.html"));
     })
