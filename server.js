998de0f67a2608a157ce9dd0ec6b0e946de03d18
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-18 14:10:13 +0530

    :(

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/posts', posts);
 
 
 
-    app.use(express.static("client/build"))
+    app.use(express.static(path.join(__dirname,"client","build")))
     app.get("*" , (req , res)=>{
         res.sendFile(__dirname + "/client/build/index.html");
     })
