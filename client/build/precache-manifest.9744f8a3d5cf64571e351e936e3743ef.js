17bd0babad6574062a402249db3ec3121cc9a0fe
mayankraghuwanshi <mayank1272@hotmail.com>
2019-01-20 20:01:29 +0530

    build was in gitignore :(

---
+++ client/build/precache-manifest.9744f8a3d5cf64571e351e936e3743ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1aa9f939a44c5d916df9",
+    "url": "/static/js/main.1aa9f939.chunk.js"
+  },
+  {
+    "revision": "9eb1f1372482bdd370bb",
+    "url": "/static/js/1.9eb1f137.chunk.js"
+  },
+  {
+    "revision": "1aa9f939a44c5d916df9",
+    "url": "/static/css/main.d0b77692.chunk.css"
+  },
+  {
+    "revision": "8a94e1bbe38bf2eadc9610b06ae47aed",
+    "url": "/index.html"
+  }
+];
